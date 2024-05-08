67ca6c2f98b4f45583728dcd379e841f5ddd0782
tungbeooo89 <kafkailterpun0suy@hotmail.com>
2024-05-08 17:49:45 +0700

    demo project

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function productExceptSelf(nums) {
-  const n = nums.length;
-  const result = new Array(n).fill(1);
-  let product = 1;
-  for (let i = 0; i < n; i++) {
-    result[i] *= product;
-    product *= nums[i];
-  }
-  product = 1;
-  for (let i = n - 1; i >= 0; i--) {
-    result[i] *= product;
-    product *= nums[i];
-  }
-  return result;
-}
+const initial = (arr) => arr.slice(0, -1);
