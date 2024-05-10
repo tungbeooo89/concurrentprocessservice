4a299d1e9bb15cfb2c2687dd9d3ef230f5b470e4
tungbeooo89 <kafkailterpun0suy@hotmail.com>
2024-05-11 04:10:59 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function add(x, y) {
-  return x + y;
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
+  for (let i = 0; i < envelopes.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (envelopes[i][1] > envelopes[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
 }
-
-const result = add(5, 3);
-console.log(result); // 8
